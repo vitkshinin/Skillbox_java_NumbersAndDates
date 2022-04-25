424ed572ac06cd0ce24657fdb76bf8c761d2b799
Konstantin Shibkov <shibkov.k@gmail.com>
2022-04-25 22:20:03 +0500

    init

---
+++ src/main/java/practice/TrucksAndContainers.java
@@ -0,0 +1,28 @@
+package practice;
+
+import java.util.Scanner;
+
+public class TrucksAndContainers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //получение количество коробок от пользователя
+        int boxes = scanner.nextInt();
+
+        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+        // пример вывода при вводе 2
+        // для отступа используйте табуляцию - \t
+
+        /*
+        Грузовик: 1
+            Контейнер: 1
+                Ящик: 1
+                Ящик: 2
+        Необходимо:
+        грузовиков - 1 шт.
+        контейнеров - 1 шт.
+        */
+    }
+
+}
