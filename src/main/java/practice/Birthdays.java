268bb3dc095bf3a0755ab1ef7091a2794ef4d2a5
Vitaly Kshinin <vitaly.kshinin@gmail.com>
2022-10-13 17:55:51 +0300

    Homework 8 done

--- src/main/java/practice/Birthdays.java
+++ src/main/java/practice/Birthdays.java
@@ -1,5 +1,9 @@
 package practice;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Birthdays {
 
     public static void main(String[] args) {
@@ -14,10 +18,28 @@ public class Birthdays {
 
     public static String collectBirthdays(int year, int month, int day) {
 
+        int a = 0;
+
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E", Locale.ENGLISH);
+
+        String text = System.lineSeparator();
+
+        while ((birthday.isBefore(today)) || birthday.isEqual(today)) {
+
+            text = text + a + " - " + formatter.format(birthday) + System.lineSeparator();
+            birthday = birthday.plusYears(1);
+            a = a + 1;
+        }
+        if (birthday.isEqual(today)) {
+            text = a + "-" + formatter.format(today);
+        }
+        return text;
+
+
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
     }
 }
--- src/main/java/practice/TrucksAndContainers.java
+++ src/main/java/practice/TrucksAndContainers.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 public class TrucksAndContainers {
 
+    private static final int BOX_IN_CONTAINER = 27;
+    private static final int CONTAINER_IN_TRUCK = 12;
+
     public static void main(String[] args) {
+
+        int truck = 0;
+        int container = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         //получение количество коробок от пользователя
@@ -23,6 +30,20 @@ public class TrucksAndContainers {
         грузовиков - 1 шт.
         контейнеров - 1 шт.
         */
+
+        for(int i = 0; i < boxes;) {
+            if(i % (BOX_IN_CONTAINER * CONTAINER_IN_TRUCK) == 0) {
+                truck++;
+                System.out.println("Грузовик: " + truck);
+            }
+                if(i % BOX_IN_CONTAINER == 0) {
+                    container ++;
+                    System.out.println("\t" + "Контейнер: " + container);
+                }
+                i++;
+                System.out.println("\t\t" + "Ящик: " + i);
+        }
+        System.out.println("Необходимо:" + "\n" + "грузовиков - " + truck + " шт." + "\n" + "контейнеров - " + container + " шт.");
     }
 
 }
